d31af0c9158dcff558c7338b5b7933d34603a5ca
SandeepKushwaha <sandeepkush327@gmail.com>
2023-06-30 12:09:16 +0530

    expending to multiple files

--- index.js
+++ index.js
@@ -28,20 +28,44 @@ function requestHandler(req, res) {
     // res.end('<h1>Gotcha!</h1>');
 
     // setting the response as HTML file
-    res.writeHead(200, { 'content-type': 'text/html' });
-    fs.readFile('./index.html', function (err, data) {
-        if (err) {
-            console.log('error', err);
-            return res.end('<h1>Error!</h1>');
-        }
+    // res.writeHead(200, { 'content-type': 'text/html' });
+    // fs.readFile('./index.html', function (err, data) {
+    //     if (err) {
+    //         console.log('error', err);
+    //         return res.end('<h1>Error!</h1>');
+    //     }
 
-        return res.end(data);
-    });
+    //     return res.end(data);
+    // });
 
     // install nodemon (npm install -g nodemon) and
     // start the server with (nodemon index.js) for
     // auto change js and restart the server.
     // NOTE: nodemon watches only file with extension: js, mjs, json
+
+    // Extending to multiple Pages
+    res.writeHead(200, { 'content-type': 'text/html' });
+    let filePath;
+    switch (req.url) {
+        case '/':
+            filePath = './index.html';
+            break;
+        case '/profile':
+            filePath = './profile.html';
+            break;
+        default:
+            filePath = './404.html';
+            break;
+    }
+
+    fs.readFile(filePath, function (err, data) {
+        if (err) {
+            console.log('error:', err);
+            res.end('<h1>Error!</h1>');
+        }
+        
+        return res.end(data);
+    }); 
 }
 
 // setting request handler on createServer 
